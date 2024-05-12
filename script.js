af646cbf4569f408299e52efd7814e0a4f17fd00
TheDolphinCoder11 <153998936+TheDolphinCoder11@users.noreply.github.com>
2024-05-12 10:09:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function closeAndOpenN1() {
+    window.open("./blog.html");
+    window.close("./index.html");
+};
+
+function openAndCloseN2() {
+    window.open("./certifications.html");
+    window.close("./index.html");
+};
+
+// ----------------------------------------------->
+
+// This function is only for desperate situations ONLY, then self-destruct. G3N8K0L2C1WD3942
+
+function sd() {
+    window.close("./index.html");
+    window.close("./certifications.html");
+};
+
