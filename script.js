b3a6c79f17ad19bd7dbe8ff787802aa487f1fd6a
TheDolphinCoder11 <153998936+TheDolphinCoder11@users.noreply.github.com>
2024-07-26 22:57:25 +0800

    Added functionality to buttons

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function closeAndOpenN1() {
-    window.open("./blog.html");
-    window.close("./index.html");
-};
+function openInstagram() {
+    window.open("https://www.instagram.com/kylerissocool11/")
+}
 
-function openAndCloseN2() {
-    window.open("./certifications.html");
-    window.close("./index.html");
-};
+function openThreads() {
+    window.open("https://www.threads.net/@kylerissocool11")
+}
 
+function openGmail() {
+    window.open("https://mail.google.com/mail/?view=cm&fs=1&to=kyler.chanpinhan@gmail.com")
+}
+
+function openGitHub() {
+    window.open("https://github.com/TheDolphinCoder11")
+}
